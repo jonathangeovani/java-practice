875c5241c516de428bc168417d5c20d544d39f3e
jonathangeovani <dongeovanimenor@gmail.com>
2023-09-05 12:12:14 -0300

    create IfStatement file

---
+++ exercises/IfStatements/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your age:");
+        int age = scanner.nextInt();
+
+        if (age >= 75) {
+            System.out.println("You are a Boomer!");
+        } else if (age >= 18) {
+            System.out.println("You are an Adult!");
+        } else {
+            System.out.println("You are not an Adult!");
+        }
+    }
+}
