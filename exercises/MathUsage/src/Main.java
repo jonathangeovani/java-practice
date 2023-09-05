fcd6ceba45fcf08bcd70dda68043f5f6c15c0c6e
jonathangeovani <dongeovanimenor@gmail.com>
2023-09-05 12:11:31 -0300

    create MathUsage file

---
+++ exercises/MathUsage/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double x;
+        double y;
+        double z;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the side x:");
+        x = scanner.nextDouble();
+        System.out.println("Enter the side y:");
+        y = scanner.nextDouble();
+
+        z = Math.sqrt((x*x)+(y*y));
+        System.out.println("The hypotenuse is: "+z);
+
+        scanner.close(); // is a good practice to close the scanner;
+    }
+}
