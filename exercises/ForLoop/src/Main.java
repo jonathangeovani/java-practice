36e4226fe845d6936b3c27ec5eee2684dd11d8a8
jonathangeovani <dongeovanimenor@gmail.com>
2023-09-05 17:52:21 -0300

    create ForLoop file

---
+++ exercises/ForLoop/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 3; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Happy new year!");
+    }
+}
