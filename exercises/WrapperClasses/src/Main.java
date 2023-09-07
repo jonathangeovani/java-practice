472ce478b1832a64325bda334621091290db5847
jonathangeovani <dongeovanimenor@gmail.com>
2023-09-07 13:10:45 -0300

    create WrapperClasses file

---
+++ exercises/WrapperClasses/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // Wrapper classes =    provides a way to use primitive data types as reference data types
+        //                      reference data types contains useful methods
+        //                      can be used with collections (ex.ArrayList)
+
+        // primitive    wrapper
+        // ---------    -------
+        // boolean      Boolean
+        // char         Character
+        // int          Integer
+        // double       Double
+
+        // autoboxing = the automatic conversion that the Java compiler makes between primitive data types and their corresponding wrapper class
+        // unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primitive
+
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Awesome";
+    }
+}
