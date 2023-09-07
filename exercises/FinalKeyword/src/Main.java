c66967af63f57fb896bb74c0d75f89d530842dda
jonathangeovani <dongeovanimenor@gmail.com>
2023-09-07 19:02:52 -0300

    create FinalKeyword file

---
+++ exercises/FinalKeyword/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // final variables can't be changed.
+        final double PI = 3.14159; // Good practice to name final variables with upper case letters
+
+        System.out.println(PI);
+    }
+}
