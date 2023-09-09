df4c73576099a0fefd78991eb67bdd2bdec90d6f
jonathangeovani <dongeovanimenor@gmail.com>
2023-09-09 14:43:13 -0300

    create Constructors file

---
+++ exercises/Constructors/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Human rick = new Human("Rick",65,70);
+        Human morty = new Human("Morty", 16, 50);
+
+        rick.eat();
+        morty.drink();
+    }
+}
