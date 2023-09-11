c24d97c8c3372ae4a1aabbba32a928d44ba55e02
jonathangeovani <dongeovanimenor@gmail.com>
2023-09-11 16:42:57 -0300

    create Interface file

---
+++ exercises/Interface/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // interface =  a template that can be applied to a class.
+        //              similar to inheritance, but specifies what a class must has/must do.
+        //              classes can apply more than one interface, inheritance is limited to 1 super
+
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
+        Fish fish = new Fish();
+
+        rabbit.flee();
+        hawk.hunt();
+
+        fish.hunt();
+        fish.flee();
+    }
+}
