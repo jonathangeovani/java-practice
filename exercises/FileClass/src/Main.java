5b141b936905e2771383a76dce54d779de9737c3
jonathangeovani <dongeovanimenor@gmail.com>
2023-09-12 10:53:48 -0300

    create FileClass file

---
+++ exercises/FileClass/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // file =   An abstract representation of file and directory pathname
+
+        File file = new File("secret_message.txt");
+
+        if (file.exists()) {
+            System.out.println("That file exists! :0!");
+            System.out.println(file.getPath());
+            System.out.println(file.getAbsolutePath());
+            System.out.println(file.isFile());
+            //file.delete();
+        } else {
+            System.out.println("That file doesn't exist! :(");
+        }
+    }
+}
